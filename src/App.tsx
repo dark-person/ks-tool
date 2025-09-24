3a29b493bda1c0b577f9b0abf31b2222c6b00241
dark-person <66205094+dark-person@users.noreply.github.com>
2025-09-24 23:15:18 +0800

    Use dark mode and shadcn button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@/components/theme-provider"
+import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import "./App.css"
 
@@ -5,16 +7,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((cnt) => cnt + 1)}>count is {count}</button>
+        <Button onClick={() => setCount((cnt) => cnt + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </ThemeProvider>
   )
 }
 
