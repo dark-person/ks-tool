a67afe2b7cb0234a5e7e3c6c6ac6353c8e14ae99
dark-person <66205094+dark-person@users.noreply.github.com>
2025-09-25 01:00:05 +0800

    Update vite setting and title

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ks-tool/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
